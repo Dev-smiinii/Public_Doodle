1d483e8c2b36e001a676973df8d1cce6953e8429
dev1smiinii <sam.sangmin.jeon@gmail.com>
2023-08-29 18:11:44 +0900

    git init(first) commit & 230829

---
+++ Doodle/001/01.js
@@ -0,0 +1,557 @@
+/* 1. If문
+if (condition1) {}
+if (boolean true|false) {}
+if (condition3) {}
+if (boolean true|false) {}
+if (condition5) {}
+if (boolean true|false) {}
+if (condition7) {}
+if (boolean true|false) {}
+if (condition9) {}
+if (boolean true|false) {}
+*/
+
+/* 2. If..else문
+if (condition1) {} else {}
+if (boolean true|false) {} else {}
+if (condition3) {} else {}
+if (boolean true|false) {} else {}
+if (condition5) {} else {}
+if (boolean true|false) {} else {}
+if (condition7) {} else {}
+if (boolean true|false) {} else {}
+if (condition9) {} else {}
+if (boolean true|false) {} else {}
+*/
+
+/* 3. For문
+for (let i = 0; i < arr.length; i++) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+for (let i = 1 ; i < arr.length; i++ ) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+for (let i = 2; i < arr.length; i++) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+for (let i = 3; i < arr.length; i++) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+for (let i = 4; i < arr.length; i++) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+for (let i = 5; i < arr.length; i++) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+for (let i = 6; i < arr.length; i++) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+for (let i = 7; i < arr.length; i++) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+for (let i = 8; i < arr.length; i++) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+for (let i = 9; i < arr.length; i++) {
+	const elem = arr[i]
+	console.log(arr[i])
+}
+*/
+
+/* 4. For..of문
+for (const iterator1 of object) {
+	console.log(iterator1)
+}
+for (const iterator2 of object) {
+	console.log(iterator2)
+}
+for (const iterator3 of object) {
+	console.log(iterator3)
+}
+for (const iterator4 of object) {
+	console.log(iterator4)
+}
+for (const iterator5 of object) {
+	console.log(iterator5)
+}
+for (const iterator6 of object) {
+	console.log(iterator6)
+}
+for (const iterator7 of object) {
+	console.log(iterator7)
+}
+for (const iterator8 of object) {
+	console.log(iterator8)
+}
+for (const iterator9 of object) {
+	console.log(iterator9)
+}
+for (const iterator10 of object) {
+	console.log(iterator10)
+}
+*/
+
+/* 5. Function
+function name1(params) {
+	// this
+	// return this
+}
+function name2(params) {
+	// this
+	// return undefined
+}
+function name3(params) {
+	// this
+	// return this
+}
+function name4(parmas) {
+	// this
+	// return undefined
+}
+function name5(parmas) {
+	// this
+	// return this
+}
+function name6(parmas) {
+	// this
+	// return undefined
+}
+function name7(parmas) {
+	// this
+	// return this
+}
+function name8(parmas) {
+	// this
+	// return undefined
+}
+function name9(parmas) {
+	// this
+	// return this
+}
+function name10(parmas) {
+	// this
+	// return undefined
+}
+*/
+
+/* 6. Function & Exec
+const a1 = function () {
+	// return undefined
+}
+a1()
+const b2 = function () {
+	// return undefined
+}
+b2()
+const c3 = function () {
+	// return undefined
+}
+c3()
+const d4 = function () {
+	// return undefined
+}
+d4()
+const e5 = function () {
+	// return undefined
+}
+e5()
+const f6 = function () {
+	// return undefined
+}
+f6()
+const g7 = function () [
+	// return undefined
+]
+g7()
+const h8 = function () {
+	// return undefined
+}
+h8()
+const i9 = function () {
+	// return undefined
+}
+i9()
+const j10 = function () {
+	// return undefined
+}
+j10()
+*/
+
+/* 7. A.Function & Exec
+const a11 = () => {
+	// return undefined
+}
+a11()
+const b21 = () => {
+	// return undefined
+}
+b21()
+const c31 = () => {
+	// return undefined
+}
+c31()
+const d41 = () => {
+	// return undefined
+}
+d41()
+const e51 = () => {
+	// return undefined
+}
+e51()
+const f61 = () => {
+	// return undefined
+}
+f61()
+const g71 = () => {
+	// return undefined
+}
+g71()
+const h81 = () => {
+	// return undefined
+}
+h81()
+const i91 = () => {
+	// return undefined
+}
+i91()
+const j101 = () => {
+	// return undefined
+}
+j101()
+*/
+
+/* 8. require("fs")
+const fs1 = require("fs")
+const fs2 = require("fs")
+const fs3 = require("fs")
+const fs4 = require("fs")
+const fs5 = require("fs")
+const fs6 = require("fs")
+const fs7 = require("fs")
+const fs8 = require("fs")
+const fs9 = require("fs")
+const fs10 = require("fs")
+*/
+
+/* 9. fs.readFile()
+fs.readFile()
+fs.readFile()
+fs.readFile()
+fs.readFile()
+fs.readFile()
+fs.readFile()
+fs.readFile()
+fs.readFile()
+fs.readFile()
+fs.readFile()
+*/
+
+/* 10. fs.readFile("String path", callback())
+fs.readFile("String path1", (err, data) => {})
+fs.readFile("String path2", (err, data) => {})
+fs.readFile("String path3", (err, data) => {})
+fs.readFile("String path4", (err, data) => {})
+fs.readFile("String path5", (err, data) => {})
+fs.readFile("String path6", (err, data) => {})
+fs.readFile("String path7", (err, data) => {})
+fs.readFile("String path8", (err, data) => {})
+fs.readFile("String path9", (err, data) => {})
+fs.readFile("String path10", (err, data) => {})
+*/
+
+/* 11. fs.readFile() - console.log() err & data
+fs.readFile("./1", (err, data) => {
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err) 
+})
+fs.readFile("./2", (err, data) => {
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err)
+})
+fs.readFile("./3", (err, data) =>{
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err)
+})
+fs.readFile("./4", (err,data) => {
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err)
+})
+fs.readFile("./5", (err, data) => {
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err)
+})
+fs.readFile("./6", (err, data) => {
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err)
+})
+fs.readFile("./7", (err, data) => {
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err)
+})
+fs.readFile("./8", (err, data) => {
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err)
+})
+fs.readFile("./9", (err, data) => {
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err)
+})
+fs.readFile("./10", (err, data) => {
+	if (err) throw err
+	console.log(data)
+	console.log(data.toString())
+	console.log("err :", err)
+})
+*/
+
+/* 12. fs.writeFile()
+fs.writeFile()
+fs.writeFile()
+fs.writeFile()
+fs.writeFile()
+fs.writeFile()
+fs.writeFile()
+fs.writeFile()
+fs.writeFile()
+fs.writeFile()
+fs.writeFile()
+*/
+
+/* 13. fs.writeFile("FILE_NAME", "String data", callback())
+fs.writeFile("FILE_NAME1", "String data", (err) => {})
+fs.writeFile("FILE_NAME2", "String data", (err) => {})
+fs.writeFile("FILE_NAME3", "String data", (err) => {})
+fs.writeFile("FILE_NAME4", "Stirng data", (err) => {})
+fs.writeFile("FILE_NAME5", "String data", (err) => {})
+fs.writeFile("FILE_NAME6", "String data", (err) => {})
+fs.writeFile("FILE_NAME7", "String data", (err) => {})
+fs.writeFile("FILE_NAME8", "String data", (err) => {})
+fs.writeFile("FILE_NAME9", "String data", (err) => {})
+fs.writeFile("FILE_NAME10", "String data", (err) => {})
+*/
+
+/* 14. fs.writeFile() - console.log() err
+fs.writeFile("FILE_NAME1", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Success")
+})
+fs.writeFile("FILE_NAME2", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Success")
+})
+fs.writeFile("FILE_NAME3", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Success")
+})
+fs.writeFile("FILE_NAME4", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Success")
+})
+fs.writeFile("FILE_NAME5", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Success")
+})
+fs.writeFile("FILE_NAME6", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Sucess")
+})
+fs.writeFile("FILE_NAME7", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Success")
+})
+fs.writeFile("FILE_NAME8", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Success")
+})
+fs.writeFile("FILE_NAME9", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Success")
+})
+fs.writeFile("FILE_NAME10", "String data", (err) => {
+	if (err) throw err
+	console.log("Cr8 Success")
+})
+*/
+
+/* 15. require("path") & FILE_NAME & filepath
+const path = require("path")
+const FILE_NAME1 = "String"
+const filepath1 = path.join(__dirname, __filename)
+const path = require("path")
+const FILE_NAME2 = "String"
+const filepath2 = path.join(__dirname, __filename)
+const path = require("path")
+const FILE_NAME3 = "String"
+const filepath3 = path.join(__dirname, __filename)
+const path = require("path")
+const FILE_NAME4 = "String"
+const filepath4 = path.join(__dirname, __filename)
+const path = require("path")
+const FILE_NAME5 = "String"
+const filepath5 = path.join(__dirname, __filename)
+const path = require("path")
+const FILE_NAME6 = "String"
+const filepath6 = path.join(__dirname, __filename)
+const path = require("path")
+const FILE_NAME7 = "String"
+const filepath7 = path.join(__dirname, __filename)
+const path = require("path")
+const FILE_NAME8 = "String"
+const filepath8 = path.join(__dirname, __filename)
+const path = require("path")
+const FILE_NAME9 = "String"
+const filepath9 = path.join(__dirname, __filename)
+const path = require("path")
+const FILE_NAME10 = "String"
+const filepath10 = path.join(__dirname, __filename)
+*/
+
+/* 16. fs.writeFile() + fs.readFile()
+fs.writeFile("FILE_NAME1", "String data", (err) => {
+	if (err) throw err
+	
+	fs.readFile("String path1", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString())
+
+		console.log("Cr8 Success")
+	})
+})
+fs.writeFile("FILE_NAME2", "String data", (err) => {
+	if (err) throw err
+
+	fs.readFile("String path2", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString())
+
+		console.log("Cr8 Success")
+	})
+})
+fs.writeFile("FILE_NAME3", "String data", (err) => {
+	if (err) throw err
+
+	fs.radFile("String path3", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString())
+
+		console.log("Cr8 Success")
+	})
+})
+fs.writeFile("FILE_NAME4", "String data", (err) => {
+	if (err) throw err
+
+	fs.readFile("String path4", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString())
+
+		console.log("Cr8 Success")
+	})
+})
+fs.writeFile("FILE_NAME5", "String data", (err) => {
+	if (err) throw err
+
+	fs.readFile("String path5", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString())
+
+		console.log("Cr8 Success")
+	})
+})
+fs.writeFile("FILE_NAME6", "String data", (err) => {
+	if (err) throw err
+
+	fs.readFile("String path6", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString())
+
+		console.log("Cr8 Success")
+	})
+})
+fs.writeFile("FILE_NAME7", "String data", (err) => {
+	if (err) throw err
+
+	fs.readFile("String path7", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString())
+
+		console.log("Cr8 Success")
+	})
+})
+fs.writeFile("FILE_NAME8", "String data", (err) => {
+	if (err) throw err
+
+	fs.readFile("String path8", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString())
+
+		console.log("Cr8 Success")
+	})
+})
+fs.writeFile("FILE_NAME9", "String data", (err) => {
+	if (err) throw err
+
+	fs.readFile("String path9", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString())
+
+		console.log("Cr8 Success")
+	})
+})
+fs.writeFile("FILE_NAME10", "String data", (err) => {
+	if (err) throw err
+
+	fs.readFile("String path10", (err, data) => {
+		if (err) throw err
+		console.log("err :", err)
+		console.log(data)
+		console.log(data.toString()
+
+		console.log("Cr8 Success"))
+	})
+})
+*/
